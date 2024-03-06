137e793d5a59a4a11dabcec8b050ded5c4a1de9c
willem_uu_ubuntu22.04 <wimjoost@gmail.com>
2024-03-06 18:12:29 +0100

    PP-49: fix increment version

--- .circleci/bash-increment-version.bash
+++ .circleci/bash-increment-version.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
+cd ..
 
 # Source the bash-increment-version-function.sh script
-source bash-increment-version-function.bash > /dev/null
+source ./circleci/bash-increment-version-function.bash > /dev/null
 
 # Get the current version using Maven
-cd ..
 CURRENT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
 # Split the current version into major, minor, and subversion parts
@@ -15,15 +15,15 @@ NEXT_VERSION=$(increment_version "${major}" "${minor}" "${subversion}")
 echo "${NEXT_VERSION}"
 
 # Set the new version using Maven
-#mvn versions:set -DnewVersion="${NEXT_VERSION}"
+mvn versions:set -DnewVersion="${NEXT_VERSION}"
 
 # Configure git user email and name
-#git config --global user.email "${USER_EMAIL}"
-#git config --global user.name "${USER_NAME}"
+git config --global user.email "${USER_EMAIL}"
+git config --global user.name "${USER_NAME}"
 
 # Commit the version change
-#git commit -am "Bump version to ${NEXT_VERSION} [ci skip]"
+git commit -am "Bump version to ${NEXT_VERSION} [ci skip]"
 
 # Push the changes to the remote repository
-#git push origin master
+git push origin master
 
