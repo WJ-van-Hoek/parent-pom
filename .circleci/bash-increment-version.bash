31af39da129eb2b6dbe5b66a60f514d3433abce4
willem_uu_ubuntu22.04 <wimjoost@gmail.com>
2024-03-06 15:38:16 +0100

    Merge master

--- .circleci/bash-increment-version.bash
+++ .circleci/bash-increment-version.bash
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-increment_version() {
-    awk -F'.' '{
-        if ($2 >= 9 && $3 >= 9) {
-            print $1+1".0.0"
-        } else if ($2 >= 9) {
-            print $1+1".0."$3
-        } else if ($3 >= 9) {
-            print $1"."$2+1".0"
-        } else {
-            print $1"."$2"."$3+1
-        }
-    }'
-}
+# Source the bash-increment-version-function.sh script
+source bash-increment-version-function.bash
 
+# Get the current version using Maven
 CURRENT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-NEXT_VERSION=$(echo "${CURRENT_VERSION}" | increment_version_using_awk)
+
+# Split the current version into major, minor, and subversion parts
+split_version "${CURRENT_VERSION}"
+
+# Increment the version using the split parts
+NEXT_VERSION=$(increment_version "${major}" "${minor}" "${minor}")
+
+# Set the new version using Maven
 mvn versions:set -DnewVersion="${NEXT_VERSION}"
 
+# Configure git user email and name
 git config --global user.email "${USER_EMAIL}"
 git config --global user.name "${USER_NAME}"
+
+# Commit the version change
 git commit -am "Bump version to ${NEXT_VERSION} [ci skip]"
+
+# Push the changes to the remote repository
 git push origin master
+
