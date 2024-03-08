3c556e3973b7380e7680e3e5b6dce329a40436e4
WJ-van-Hoek <124786983+WJ-van-Hoek@users.noreply.github.com>
2024-03-08 22:16:36 +0100

    fix bash-update-dependencies [ci skip]

--- .circleci/bash-update-dependencies.bash
+++ .circleci/bash-update-dependencies.bash
@@ -2,7 +2,7 @@
 
 source ./.circleci/bash-update-release-notes.bash  > /dev/null
 
-git checkout AUTO-UPDATE-DEPENDENCIES
+git checkout -b AUTO-UPDATE-DEPENDENCIES
 git pull origin master
 
 mvn versions:update-properties
@@ -14,7 +14,7 @@ git config --global user.email "${USER_EMAIL}"
 git config --global user.name "${USER_NAME}"
 
 git commit -am "Automated versions:update-properties"
-git push origin
+git push origin AUTO-UPDATE-DEPENDENCIES --set-upstream
 
 curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${_WRITE_PR}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/WJ-van-Hoek/parent-pom/pulls -d '{"title":"AUTO-PR: update properties","head":"AUTO-UPDATE-DEPENDENCIES","base":"master"}'
 
