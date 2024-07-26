bede3da0dbb0771f3574460e96e79e720cab3bf8
WJ-van-Hoek <124786983+WJ-van-Hoek@users.noreply.github.com>
2024-07-26 08:36:28 +0200

    update bash-increment-version.bash

--- .circleci/bash-increment-version.bash
+++ .circleci/bash-increment-version.bash
@@ -14,7 +14,7 @@ NEXT_VERSION=$(increment_minor_sub_version "${major}" "${minor}" "${subversion}"
 echo "${NEXT_VERSION}"
 
 # Set the new version using Maven
-mvn versions:set -DnewVersion="${NEXT_VERSION}"
+mvn  -s .circleci/settings.xml versions:set -DnewVersion="${NEXT_VERSION}"
 
 # Configure git user email and name
 git config --global user.email "${USER_EMAIL}"
